99d4f5abec0b30b823a9b989fd80267b7cb29113
k1complete <oda@maqcsa.net>
2014-10-10 21:43:08 +0900

    doc-elixir-v1.0.1

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule L10nElixir.Mixfile do
 
   def project do
     [app: :l10n_elixir,
-     version: "0.0.2",
-     elixir: "~> 1.0.0",
+     version: "0.0.3",
+     elixir: "~> 1.0.1",
      compilers: Mix.compilers ++ [:po],
      deps: deps]
   end
